5652810bf1f2c9bc712f89a1586d65b95553827b
nafiz <naafizrahman1@gmail.com>
2020-06-09 20:01:29 +0600

    Profile Activity added

--- app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileFragment.kt
@@ -1,32 +1,47 @@
 package com.example.mvvmsampleapp.ui.home.profile
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmsampleapp.R
-
-class ProfileFragment : Fragment() {
+import com.example.mvvmsampleapp.databinding.ProfileFragmentBinding
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
+import org.kodein.di.android.x.kodein
+class ProfileFragment : Fragment(),KodeinAware {
 
     companion object {
         fun newInstance() = ProfileFragment()
     }
-
     private lateinit var viewModel: ProfileViewModel
+    override val kodein: Kodein by kodein()
+    private val factory: ProfileViewModelFactory by instance<ProfileViewModelFactory>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+
+        val binding: ProfileFragmentBinding = DataBindingUtil.inflate(inflater,R.layout.profile_fragment,container,false)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        viewModel = ViewModelProvider(this,factory).get(ProfileViewModel::class.java)
+    }
 }
--- app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.mvvmsampleapp.ui.home.profile
 
 import androidx.lifecycle.ViewModel
+import com.example.mvvmsampleapp.data.repository.UserRepository
 
-class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProfileViewModel(
+    repository: UserRepository
+) : ViewModel() {
+    val user = repository.getUser()
 }
